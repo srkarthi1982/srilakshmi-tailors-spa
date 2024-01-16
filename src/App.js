615f54f2b874aea4e9b25b696b47fd47ab52f95c
srkarthi1982 <srkarthi1982@gmail.com>
2024-01-16 17:15:57 +0400

    added routes to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div>
-      Srilakshmi Tailors App
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
+
