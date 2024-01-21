2e8f14b6e431d2fe254be6f6a131dff18534c3ef
srkarthi1982 <srkarthi1982@gmail.com>
2024-01-21 21:43:40 +0400

    Updates

---
+++ static/js/289.6acfd524.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksrilakshmi_tailors_spa=self.webpackChunksrilakshmi_tailors_spa||[]).push([[289],{289:s=>{s.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.6acfd524.chunk.js.map
