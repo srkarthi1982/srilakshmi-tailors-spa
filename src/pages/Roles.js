f27f4f468de78238b263ea32b9b3c95d1023a895
srkarthi1982 <srkarthi1982@gmail.com>
2024-01-23 18:47:14 +0400

    action added

--- src/pages/Roles.js
+++ src/pages/Roles.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import supabase from '../config/supabaseClient';
-import { CloseButton, Group, Input, SimpleGrid, Text } from '@mantine/core';
-import { IconArrowLeft } from '@tabler/icons-react';
+import { ActionIcon, CloseButton, Group, Input, SimpleGrid, Text } from '@mantine/core';
+import { IconArrowLeft, IconPlus, IconSortAscending, IconSortDescending } from '@tabler/icons-react';
 import { useNavigate } from 'react-router-dom';
 import CardStrip from '../components/cardStrip';
 import CardStripSkeleton from '../components/cardStripSkeleton';
 function Roles() {
     const [roles, setRoles] = useState([]);
     const [value, setValue] = useState('');
+    const [ascending, setAscending] = useState(true);
     const navigate = useNavigate();
     const onAction = function (action, data) {
         console.log('action', action, data);
     }
     useEffect(() => {
         (async () => {
-            const { data, error } = await supabase.from('roles').select('*');
+            const { data, error } = await supabase.from('roles').select('*').order('name', { ascending });
             if (error) return;
             setRoles(data);
         })();
@@ -27,11 +28,34 @@ function Roles() {
                     <Text fw={700}>Roles ({roles.length})</Text>
                 </Group>
                 <Group>
+                    <ActionIcon.Group>
+                        <ActionIcon aria-label="Sorting" variant='default' onClick={async () => {
+                            const { data, error } = await supabase.from('roles').select('*').order('name', { ascending: !ascending });
+                            if (error) return;
+                            setRoles(data);
+                            setAscending(!ascending)
+                        }}>
+                            {ascending ? <IconSortAscending /> : <IconSortDescending />}
+                        </ActionIcon>
+                        <ActionIcon aria-label="Sorting" variant='default' onClick={async () => { }}>
+                            <IconPlus />
+                        </ActionIcon>
+                    </ActionIcon.Group>
                     <Input
                         placeholder="Search"
                         value={value}
-                        w={180}
-                        onChange={(event) => setValue(event.currentTarget.value)}
+                        w={125}
+                        onChange={async (event) => {
+                            setValue(event.currentTarget.value);
+                            const { data, error } = await supabase
+                                .from('roles')
+                                .select('*')
+                                .ilike('name', `%${event.currentTarget?.value}%`)
+                                .order('name', { ascending: !ascending });
+                            if (error) return;
+                            setRoles(data);
+
+                        }}
                         rightSectionPointerEvents="all"
                         rightSection={
                             <CloseButton
@@ -44,10 +68,10 @@ function Roles() {
                 </Group>
             </Group>
 
-            <SimpleGrid cols={{ base: 1, md: 1, lg: 2, xl: 3 }}>
+            <SimpleGrid cols={{ base: 1, md: 1, lg: 2, xl: 3 }} spacing={5} verticalSpacing={5}>
                 {
                     roles.length === 0 ?
-                        Array.from({ length: 7 }, (_, index) => <CardStripSkeleton key={index} />) :
+                        Array.from({ length: 6 }, (_, index) => <CardStripSkeleton key={index} />) :
                         roles.map(role =>
                             <CardStrip
                                 key={role.id}
